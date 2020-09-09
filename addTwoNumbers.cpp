8c1d792df9c71900bfd764841f71c06be39a4d5b
Andres Gasper <24193043+AndGasper@users.noreply.github.com>
2020-09-09 15:34:48 -0700

    Working solution from leetcode
    had to look at the Java version of the solution and work backwards.
    
    I really struggled with the list node 
    
    Not so much the new Struct returns a Struct* part, but I really did stumble on the fact that new ListNode(0) inserts a node first with a value of 0, so I really kept stumbling over like {0, val1, val2, ..., val3} 
    
    And I only kind of sort of get this idea of going, "Okay, cool, have your 0 value, imma just lop it off in the end."

--- addTwoNumbers.cpp
+++ addTwoNumbers.cpp
@@ -10,94 +10,35 @@
  */
 class Solution {
 public:
-    void workingThroughTheLogic() {
-        
-        vector<int> someValues = {1,2,3};
-        int lengthOfSomeValues = size(someValues); // starts the count at 0; 
-        
-//         for (int i = 0; i < lengthOfSomeValues; i++) {
-//             cout << "\n someValues[i]: " << someValues[i];
-//             ListNode nextNode = ListNode(someValues[i]);
-//             if (i == 0) {
-//                 ListNode exampleListNode = nextNode;
-//                 exampleListNode.next = &nextNode;
-//             } else {
-//                 // do not redeclare the solution? I couldn't figure out the initial 0 problem and thought you had to declare a variable before you use it
-//                 // so it's like???
-//                 exampleListNode.next = &nextNode;
-//             }
-//         }
-        
-        return;
-    }
-    void printValues(vector<int> values) {
-        for (int i = 0; i < size(values); i++) {
-            cout << "\n values[i]: " << values[i];
-        }
-        return;
-    }
-    ListNode* addAValueToAList(int someValue, ListNode* aList) {
-        aList->next = new ListNode(someValue, nullptr);
-        return aList;
-    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // workingThroughTheLogic();
-        int carryOver = 0;
-        int sum;
-        int resultValue;
-        vector<int> solutionValues;
-        // sum = l1.val + l2.val + carryOver
-        // carryOver = sum % 10
-        while(l1 != NULL && l2 != NULL) {
-            sum = l1->val + l2->val + carryOver;
+        // literally just trying to translate the Java solution
+        // presumably dummyHead because of the whole ->next: [3,4,2] -> [4, 2], [2], [] style business
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* copyL1 = l1;
+        ListNode* copyL2 = l2;
+        ListNode* current = dummyHead;
+        int carryOver = 0; // in case you know the value rolls over
+        int resultValue; // just for readability because it's like what if you had other based counting systems instead of 10
+        while (copyL1 != NULL || copyL2 != NULL) {
+            // well, I'll be...c++ has ternary expressions? That seems...iffy
+            int value1 = (copyL1 != NULL) ? copyL1->val : 0;
+            int value2 = (copyL2 != NULL) ? copyL2->val : 0;
+            int sum = carryOver + value1 + value2;
+            carryOver = sum /10; // seems like there's some type coercion or some such foolishness
             resultValue = sum % 10;
-            carryOver = sum / 10;
-            cout << "\n sum: " << sum;
-            cout << "\n carryOver: " << carryOver;
-            cout << "\n resultValue: " << resultValue;
-            solutionValues.push_back(resultValue);
-            
-            // l1 = l1->next; // now pointing to the next value; so it's like, [2,4,3] -> [4, 3] -> [3] -> []
-            // so there's the possibility that they're different sizes
-            l1 = l1->next;
-            l2 = l2->next;
-        }
-        // expanding brain meme level logic here.
-        ListNode* solutionList = new ListNode(solutionValues[0], nullptr);
-        cout << "\n whats the carryOver? " << carryOver;
-        // [5], [5]
-        // there's carry over
-        if (carryOver != 0) {
-            solutionList->next = new ListNode(carryOver, nullptr);
-        }
-        // l1 is shorter than l2 so add the last value of l2; ...assuming the diff is only 1 value *sigh*
-        if (l1 == NULL && l2 != NULL) {
-            sum = l2->val + carryOver;
-            resultValue = sum % 10;
-            carryOver = sum / 10;
-            solutionList->next = new ListNode(resultValue, nullptr);
-            if (carryOver != 0) {
-                solutionList->next->next = new ListNode(carryOver, nullptr);
+            // must use the new keyword to get that sweet, sweet pointer
+            current->next = new ListNode(resultValue);
+            current = current->next;
+            if (copyL1 != NULL) {
+                copyL1 = copyL1->next;
+            }
+            if (copyL2 != NULL) {
+                copyL2 = copyL2->next;
             }
         }
-        // if l2 is shorter than l1
-        if (l2 == NULL && l1 != NULL) {
-            sum = l1->val + carryOver;
-            resultValue = sum % 10;
-            solutionList->next = new ListNode(resultValue, nullptr);
-            carryOver = sum / 10;
-            if (carryOver != 0) {
-                solutionList->next->next = new ListNode(carryOver, nullptr);
-            }
+        if (carryOver > 0) {
+            current->next = new ListNode(carryOver);
         }
-        if (size(solutionValues) >= 2) {
-            solutionList->next = new ListNode(solutionValues[1], nullptr);
-        }
-        if (size(solutionValues) >= 3) {
-            solutionList->next->next = new ListNode(solutionValues[2], nullptr);
-        }
-        
-    
-        return solutionList;
+        return dummyHead->next;
     }
 };
